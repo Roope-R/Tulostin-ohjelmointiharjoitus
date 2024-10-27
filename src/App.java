f17aa4775fcdbbdf020d0914edd6693b7c5fca74
Roope Rajamäki <roope13@live.com>
2024-10-27 17:25:23 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.print("Ohjelman tekijä ");
+        String tekija;
+        tekija = "Roope";
+        System.out.println(tekija);
+
+        int luku1;
+        int luku2;
+        luku1 = 10;
+        luku2 = 5;
+       
+        System.out.println("Luku1-muuttujan arvo on 10");
+        System.out.println("Luku2-muuttujan arvo on 5");
+        int tulo;
+        tulo = 50;
+        System.out.println("10 * 5 = 50");
+        System.out.println("10 - 5 = 5");
+        System.out.println("10 + 5 = 15");
+        System.out.println("10 / 5 = 2");
+    }
+}
